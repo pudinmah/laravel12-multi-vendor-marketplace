499107ee768692f76022e0fff1b87715a2bc54ee
pudin <pudinmah69@gmail.com>
2025-07-08 14:58:18 +0700

    Memisahkan Tampilan Template

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <!-- CSS files -->
+    <link href="{{ asset('assets/admin/css/tabler.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-flags.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-payments.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/tabler-vendors.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/demo.min.css') }}" rel="stylesheet" />
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
+
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
+
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
+</head>
+
+<body>
+    <script src="{{ asset('assets/admin/js/demo-theme.min.js') }}"></script>
+    <div class="page">
+        <!-- Sidebar -->
+        @include('admin.components.sidebar')
+
+        @yield('content')
+    </div>
+
+
+
+    <!-- Tabler Core -->
+    <script src="{{ asset('assets/admin/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('assets/admin/js/demo.min.js') }}" defer></script>
+
+
+</body>
+
+</html>
